28d357de3d833e8ff2e340a847fd745273b7ceb8
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-04-22 00:37:11 +0530

    get route done

--- backend/routes/candidateRoute.js
+++ backend/routes/candidateRoute.js
@@ -17,7 +17,13 @@ router.get('/:id',jwtAuthMiddleware,async(req,res)=>{
                 id:userId
             }
         })
-        console.log(userId);
+        if(!user){
+            return res.status(404).json({
+                message:"User not found"
+            })
+        }
+        console.log(user);
+        res.status(200).json({user})
     }catch(e){
         console.log(e);
         res.status(500).json({
